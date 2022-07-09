311d89ee2260740a0bd487320eed4213c948763f
Simeon Atanasov <simeon.g.atanasov@gmail.com>
2022-07-09 19:15:22 +0300

    added some html

---
+++ sumTwoNumbers.php
@@ -0,0 +1,22 @@
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>Exercise</title>
+    </head>
+
+    <body>
+        <form>
+            <label for="firstNumber">your first value:</label>
+            <input type="text" id="firstNumber"><br>
+            <label for="secondNumber">your second value:</label>
+            <input type="text" id="secondNumber"><br>
+            <input type="submit" value="sum">
+        </form>
+            <label for="result">result:</label>
+            <input type="text" id="result" readonly>
+    </body>
+</html>
+
+
+<?php
+
