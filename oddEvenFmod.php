1bd7803d631e7b5d17a8ff43bf44fa5fe898f08a
Simeon Atanasov <simeon.g.atanasov@gmail.com>
2022-07-20 21:57:44 +0300

    odd, even or invalid with Fmod

---
+++ oddEvenFmod.php
@@ -0,0 +1,30 @@
+<?php
+    if ($_POST) {
+        $inputNumber = trim($_POST['inputNumber']);
+        $rem = fmod($inputNumber,2);
+        if ($rem == 0) {
+            echo "even ";
+        } else if ($rem == round($rem)) {
+            echo "odd ";
+        } else {
+            echo "invalid";
+        }
+    } else {
+        echo '<b><h2>'.'You have to fill the textbox with a number first'.'</b></h2>';
+    }
+?>
+
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>oddEvenFmod</title>
+    </head>
+    <body>
+        <form action="oddEvenFmod.php" method="post">
+            <label for="inputNumber">enter your number:</label>
+            <input type="text" id="inputNumber" name="inputNumber"><br>
+            <input type="submit" value="submit">
+        </form>
+    </body>
+</html>
+
