dd4ee31e588e22c5cef2d2a860300595ace0b9af
Simeon Atanasov <simeon.g.atanasov@gmail.com>
2022-07-23 14:34:25 +0300

    colourful odd/even numbers

--- colourfulNumbers.php
+++ colourfulNumbers.php
@@ -1,5 +1,9 @@
 <?php
+    // TASK: Print all numbers from 1 to n and display Odd in blue, Even in green
+    // Use ul li span
 
+/*
+    // Method 1
     $num = 10;
     for ($i=1; $i <= $num; $i++) {
         echo '<ul>';
@@ -8,5 +12,18 @@
         } else {
             echo '<li>' . '<span style="color:blue">' . $i . '</span>' . '</li>';
         }
+        echo '</ul>';
+    }
+*/
+    // Method 2
+    $num = 10;
+
+    for ($i=1; $i <= $num; $i++) {
+    $color = 'blue';
+        echo '<ul>';
+        if ($i % 2 == 0) {
+            $color = 'green';
+        }
+        echo "<li><span style='color:$color'> $i </span></li>";
         echo '</ul>';
     }
