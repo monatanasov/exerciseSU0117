8d6685e902e5a299fea67f6f96d3f2f92f4709de
Simeon Atanasov <simeon.g.atanasov@gmail.com>
2022-07-23 11:53:48 +0300

    task comment

---
+++ fruitOrVegetable.php
@@ -0,0 +1,25 @@
+<?php
+// Print "fruit", "vegetable" or "unknown" depending on the input string
+    if($_POST) {
+
+    } else {
+        echo '<b><h2>'.'Please fill your string to determine is it Fruit, Vegetable or Unknown'.'</b></h2>';
+    }
+
+
+?>
+
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>oddEvenFmod</title>
+    </head>
+<body>
+    <form action="fruitOrVegetable.php" method="post">
+        <label for="inputString">input your string:</label>
+        <input type="text" id="inputString" name="inputString"><br>
+        <input type="submit" value="submit">
+    </form>
+    </body>
+</html>
+
